ae93eed1abef9700148294ee267df5c0db4c9cc8
Arthur Bufalo <4r7hurz1nh0@gmail.com>
2022-06-13 06:50:08 +0000

    Added an API Made the project compatible with MongoDB

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,19 @@
-import paletas from "../mock/paletas";
-import React, { useState } from "react";
+import { useState } from "react";
 import Card from "../Card";
 
 const Home = () => {
-  const [palette, setPalette] = useState(paletas);
-
+  const [palette, setPalette] = useState([]);
+  if (!palette.length)
+    fetch("http://localhost:8080/sorvetes")
+      .then(res => res.json())
+      .then(setPalette)
+      .catch(err => console.log(err));
   return (
     <div className="home-container">
       <h2>Lista de Paletas</h2>
       <div>
         {palette.map((element) => {
-          return <Card key={element.id} palette={element} />;
+          return <Card key={element._id} palette={element} />;
         })}
       </div>
     </div>
